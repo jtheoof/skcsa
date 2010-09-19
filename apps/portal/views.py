cdbcb2f1ca81e347eff08c3d06fddc9348627246
Jeremy Attali <jeremy.attali@gmail.com>
2010-09-19 15:40:53 +0200

    working on website

--- apps/portal/views.py
+++ apps/portal/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def home(request):
-    return render_to_response('portal/home.html')
+    return render_to_response('portal/home.html', context_instance=RequestContext(request))
